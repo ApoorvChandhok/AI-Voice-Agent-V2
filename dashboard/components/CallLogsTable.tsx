b60ff7bc1c943497929b3729083e1a672ae9b1f0
Apoorv Chandhok <apoorv.chandhok1999@gmail.com>
2026-07-01 17:03:32 +0530

    Fix React is not defined error in CallLogsTable

--- dashboard/components/CallLogsTable.tsx
+++ dashboard/components/CallLogsTable.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { Activity, Play, Clock, ChevronDown, ChevronUp, FileText } from "lucide-react";
 import Link from "next/link";
 
